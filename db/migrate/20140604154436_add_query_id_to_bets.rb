b08d76cd38f5d318057b9e3d708f9b2a352c4c1a
Elliot Crosby-McCullough <elliot.cm@gmail.com>
2014-06-11 13:58:30 +0100

    Alter query migration to migrate bet data.

--- db/migrate/20140604154436_add_query_id_to_bets.rb
+++ db/migrate/20140604154436_add_query_id_to_bets.rb
@@ -2,7 +2,15 @@ class AddQueryIdToBets < ActiveRecord::Migration
   def change
     add_column :bets, :query_id, :integer
     add_column :bets, :is_best, :boolean, default: true
-    remove_column :bets, :query
-    remove_column :bets, :match_type
+
+    Bet.all.each do |bet|
+      query = Query.where(query: bet['query'], match_type: bet['match_type']).first
+      query ||= Query.create!(query: bet['query'], match_type: bet['match_type'])
+
+      bet.update_attributes(is_best: bet.position.present?, query_id: query.id)
+    end
+
+    remove_column :bets, :query, :string
+    remove_column :bets, :match_type, :string
   end
 end
