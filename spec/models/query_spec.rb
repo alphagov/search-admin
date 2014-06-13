ebe57ec3efeccb882e9b029d8dd3a2e5e8e06583
Pedro Moreira <pedro.moreira@unboxedconsulting.com>
2014-06-13 15:24:41 +0100

    Add Query unit tests

--- spec/models/query_spec.rb
+++ spec/models/query_spec.rb
@@ -10,3 +10,36 @@ describe Query, 'associations' do
     expect(bets).to eq []
   end
 end
+
+describe Query, 'validations' do
+  it 'is invalid without a query attribute' do
+    attributes = FactoryGirl.attributes_for(:query, query: nil)
+
+    expect(new_query_with(attributes)).not_to be_valid
+  end
+
+  it 'validates inclusion of match_types in Query::MATCH_TYPES' do
+    attributes = FactoryGirl.
+      attributes_for(:query, match_type: 'not like the others')
+
+    expect(new_query_with(attributes)).not_to be_valid
+  end
+
+  it "is valid with a match_type of 'exact'" do
+    attributes = FactoryGirl.
+      attributes_for(:query, match_type: 'exact')
+
+    expect(new_query_with(attributes)).to be_valid
+  end
+
+  it "is valid with a match_type of 'stemmed'" do
+    attributes = FactoryGirl.
+      attributes_for(:query, match_type: 'stemmed')
+
+    expect(new_query_with(attributes)).to be_valid
+  end
+end
+
+def new_query_with(attributes)
+  Query.new(attributes)
+end
