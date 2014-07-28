525c7fdbd54889ba1147b9862a8d85cde56c1be2
Richard Boulton <richard.boulton@digital.cabinet-office.gov.uk>
2014-07-28 09:51:01 +0000

    Migrate existing queries to lower case

---
+++ db/migrate/20140728094001_lowercase_queries.rb
@@ -0,0 +1,13 @@
+class LowercaseQueries < ActiveRecord::Migration
+  def change
+    Query.all.each do |query|
+      query.update_attribute(:query, query.query.downcase)
+    end
+
+    query_groups = Query.all.group_by {|q| [q.query, q.match_type] }
+    puts "Checking for duplicated queries"
+    puts query_groups.select { |_, queries|
+      queries.count > 1
+    }
+  end
+end
