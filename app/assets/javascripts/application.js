b60d8829ad9f3f75a6dce50d55ab46d668728ebc
tijmen <tijmen@gmail.com>
2015-05-13 14:41:16 +0100

    Show iframe with search results on query page
    To make it easier to see the effects of best/worst bets, we add an
    iframe with the actual search results on the query page. The iframe
    embeds the `gov.uk/search` page.
    
    After a best bet is deleted and added it can take a little time (a
    second or so) before elasticsearch returns the correct results. The
    iframe is therefore not guaranteed to be current after an update. This
    is mitigated by adding a callout with a warning and a button to refresh
    the iframe.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,2 +1,9 @@
 //= require jquery
 //= require jquery_ujs
+
+// The Bets page contains a button to refresh the iframe.
+$('#search-results-refresh').on('click', function () {
+  var iframe = document.getElementById('search-results');
+  iframe.src = iframe.src;
+  return false;
+});
