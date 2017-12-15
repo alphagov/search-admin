eab3aeb642b5a7f7de05c76cb62a3c91d451242f
Suzanne Hamilton <suzanne.hamilton@digital.cabinet-office.gov.uk>
2017-12-15 10:24:28 +0000

    Regenerate schema.rb
    Update schema.rb by running `bundle exec rake db:migrate`.
    
    Some of Rails' defaults have changed since this file was last generated,
    which triggered these changes in schema.rb. The MySQL config in the
    database itself has not changed.

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -13,41 +12,41 @@
 
 ActiveRecord::Schema.define(version: 20170202095022) do
 
-  create_table "bets", force: :cascade do |t|
-    t.string  "link",     limit: 255
-    t.integer "position", limit: 4,     default: 1
+  create_table "bets", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string  "link"
+    t.integer "position",               default: 1
     t.text    "comment",  limit: 65535
     t.boolean "manual",                 default: false
-    t.integer "query_id", limit: 4
+    t.integer "query_id"
     t.boolean "is_best",                default: true
-    t.integer "user_id",  limit: 4
+    t.integer "user_id"
   end
 
-  create_table "queries", force: :cascade do |t|
-    t.string   "query",      limit: 255
-    t.string   "match_type", limit: 255
+  create_table "queries", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "query"
+    t.string   "match_type"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "recommended_links", force: :cascade do |t|
-    t.string  "title",       limit: 255
-    t.string  "link",        limit: 255
+  create_table "recommended_links", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string  "title"
+    t.string  "link"
     t.text    "description", limit: 65535
     t.text    "keywords",    limit: 65535
     t.text    "comment",     limit: 65535
-    t.integer "user_id",     limit: 4
+    t.integer "user_id"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string  "name",                    limit: 255
-    t.string  "email",                   limit: 255
-    t.string  "uid",                     limit: 255
-    t.string  "organisation_slug",       limit: 255
-    t.string  "permissions",             limit: 255
-    t.boolean "remotely_signed_out",                 default: false
-    t.boolean "disabled",                            default: false
-    t.string  "organisation_content_id", limit: 255
+  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string  "name"
+    t.string  "email"
+    t.string  "uid"
+    t.string  "organisation_slug"
+    t.string  "permissions"
+    t.boolean "remotely_signed_out",     default: false
+    t.boolean "disabled",                default: false
+    t.string  "organisation_content_id"
   end
 
 end
