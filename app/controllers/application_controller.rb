ae719ac9aefad01e1080e515b685db210df96ea7
Suzanne Hamilton <suzanne.hamilton@digital.cabinet-office.gov.uk>
2017-12-18 15:23:21 +0000

    Replace deprecated signon auth check
    The latest version of gds-sso deprecates `require_signin_permission!`
    because this is now handled by Signon by default.
    
    Replace this with `authenticate_user!` to ensure the user has signed in.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,5 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   include GDS::SSO::ControllerMethods
-  before_action :require_signin_permission!
+  before_action :authenticate_user!
 end
