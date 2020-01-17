b5affaed58d53fa2e369635147c75534fba4457d
Michael Walker <mike@barrucadu.co.uk>
2020-01-17 14:36:47 +0000

    Fix Rails/InverseOf

--- app/models/query.rb
+++ app/models/query.rb
@@ -6,8 +6,8 @@ class Query < ApplicationRecord
   validates :query, uniqueness: { scope: :match_type, case_sensitive: true }
 
   has_many :bets, dependent: :destroy
-  has_many :best_bets, -> { best }, class_name: "Bet"
-  has_many :worst_bets, -> { worst }, class_name: "Bet"
+  has_many :best_bets, -> { best }, class_name: "Bet", inverse_of: :query
+  has_many :worst_bets, -> { worst }, class_name: "Bet", inverse_of: :query
 
   # Use `sort_by` to prevent N+1 queries when Queries are loaded in a list.
   def sorted_best_bets
