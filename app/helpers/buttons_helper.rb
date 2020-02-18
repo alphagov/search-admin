e44608f38aa27f64f475e732d6cc0890b1d29288
kr8n3r <jani@kraner.dev>
2020-02-18 09:48:03 +0000

    Update delete_button helper
    Update classes to match the destructive button component styles
    and add option to display the wrapping form inline

--- app/helpers/buttons_helper.rb
+++ app/helpers/buttons_helper.rb
@@ -1,8 +1,8 @@
 module ButtonsHelper
-  def delete_button(text, path)
+  def delete_button(text, path, is_inline = false)
     button_to text, path,
               method: :delete,
-              class: "btn btn-danger",
-              data: { confirm: "Are you sure?" }
+              class: "gem-c-button govuk-button govuk-button--warning",
+              form_class: ("app-display-inline" if is_inline)
   end
 end
