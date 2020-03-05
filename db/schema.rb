412e5c7372ff9510ac6918e8ded684bbc0f5ce24
hannako <jessica.jones@digital.cabinet-office.gov.uk>
2020-03-05 16:28:05 +0000

    Bets have an updated_at date  - adds updated_at column to bet table  - display last edited date  - hide bets table on query show page if there aren't any

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_28_171904) do
+ActiveRecord::Schema.define(version: 2020_03_05_162117) do
 
   create_table "bets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "link"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2020_02_28_171904) do
     t.datetime "created_at"
     t.datetime "expiration_date"
     t.boolean "permanent", default: false
+    t.datetime "updated_at"
   end
 
   create_table "queries", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
