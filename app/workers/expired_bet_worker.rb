3b5611ac5e499e45954445c7dd45fe867f9c396c
Michael Walker <mike@barrucadu.co.uk>
2020-03-18 12:19:20 +0000

    Add a worker to deactivate expired bets
    I considered adding a rake task for this too, but don't think that's
    necessary - the reindex task will work.

---
+++ app/workers/expired_bet_worker.rb
@@ -0,0 +1,22 @@
+class ExpiredBetWorker
+  include Sidekiq::Worker
+
+  def perform
+    recently_expired_bets.find_each do |bet|
+      SearchApiSaver.new(bet).destroy(action: :deactivate)
+    end
+  end
+
+private
+
+  # job runs every hour but checks for bets which expired in the last
+  # 90 minutes - this is to reduce the chance of something being
+  # missed due to jitter in job start time.
+  def recently_expired_bets
+    Bet.where(
+      "expiration_date IS NOT NULL AND expiration_date >= ? AND expiration_date <= ?",
+      90.minutes.ago,
+      Time.zone.now,
+    )
+  end
+end
