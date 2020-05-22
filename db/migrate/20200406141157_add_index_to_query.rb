729fdc04034a8ca21365b8b9953016ba7e3746ac
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2020-05-22 13:01:51 +0100

    Remove redundant RuboCop config

--- db/migrate/20200406141157_add_index_to_query.rb
+++ db/migrate/20200406141157_add_index_to_query.rb
@@ -1,5 +1,5 @@
 class AddIndexToQuery < ActiveRecord::Migration[6.0]
   def change
-    add_index :queries, [:query, :match_type], unique: true
+    add_index :queries, %i[query match_type], unique: true
   end
 end
