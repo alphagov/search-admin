7c7da97b6114daeb6094b2539e05b9b4a49ae922
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2020-06-11 09:14:42 +0100

    Linting auto-correct

--- app/helpers/table_helper.rb
+++ app/helpers/table_helper.rb
@@ -1,9 +1,9 @@
 module TableHelper
   def table_for(hash)
-    content_tag :table, class: "table key-value-table" do
+    tag.table class: "table key-value-table" do
       rows = hash.map do |k, v|
-        content_tag :tr do
-          content_tag(:td, k) + content_tag(:td, display_value(v))
+        tag.tr do
+          tag.td(k) + tag.td(display_value(v))
         end
       end
 
@@ -15,8 +15,8 @@ module TableHelper
     if value.is_a?(Hash)
       table_for(value)
     elsif value.is_a?(Array)
-      content_tag :ul do
-        value.map { |ae| content_tag :li, display_value(ae) }.join.html_safe
+      tag.ul do
+        value.map { |ae| tag.li display_value(ae) }.join.html_safe
       end
     elsif value.blank?
       "<em>empty</em>".html_safe
--- app/helpers/tag_helper.rb
+++ app/helpers/tag_helper.rb
@@ -2,6 +2,6 @@ module TagHelper
   # Set the page <title> and return the <h1> tag.
   def page_title(string)
     content_for :page_title, "#{string} - Search Admin"
-    content_tag :h1, string, class: "page-title-with-border"
+    tag.h1 string, class: "page-title-with-border"
   end
 end
