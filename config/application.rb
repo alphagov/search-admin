7492332c0c0c13ec03dfa88b69bb1002c5a7c026
Celia Collins <celiadcollins@gmail.com>
2020-08-04 16:48:18 +0100

    Fix asset pipeline with libsass
    Using a sass css compressor causes a scss file to be processed twice
    (once to build, once to compress) which breaks the usage of "unquote"
    to use CSS that has same function names as SCSS such as max.

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,11 @@ module SearchAdmin
 
     config.action_view.default_form_builder = GenericFormBuilder
     config.action_view.field_error_proc = proc { |html_tag, _| html_tag }
+
+    # Using a sass css compressor causes a scss file to be processed twice
+    # (once to build, once to compress) which breaks the usage of "unquote"
+    # to use CSS that has same function names as SCSS such as max.
+    # https://github.com/alphagov/govuk-frontend/issues/1350
+    config.assets.css_compressor = nil
   end
 end
