6955382978588534fc25246cfcbb0ebe9e34dc1c
Michael Walker <mike@barrucadu.co.uk>
2021-04-19 13:33:39 +0100

    Add RFC 141 healthcheck endpoints
    I've added another check to this because it uses postgres.
    
    Once govuk-puppet has been updated, the old endpoint can be removed.
    
    See the RFC for more details.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get "/healthcheck/live", to: proc { [200, {}, %w[OK]] }
+  get "/healthcheck/ready", to: GovukHealthcheck.rack_response(
+    GovukHealthcheck::ActiveRecord,
+  )
+
   resources :bets, except: %i[index show] do
     member do
       post :deactivate
