bf369610c757febee8152586e1234534c4358b4e
Kevin Dew <kevindew@me.com>
2021-07-13 19:49:59 +0100

    Resolve Rails/FilePath violations
    Both of these methods weren't actually used so could be removed.

--- features/support/best_bets.rb
+++ features/support/best_bets.rb
@@ -133,10 +133,6 @@ def check_search_api_was_sent_a_recommended_link_delete(link:, index:)
   assert_search_api_deleted_item(link, index: index)
 end
 
-def run_best_bets_elasticsearch_exporter
-  `#{"#{Rails.root}bin/export_best_bets_for_elasticsearch"}`
-end
-
 def confirm_best_bets_elasticsearch_format(dump, queries)
   queries.each do |query|
     es_doc_header = {
