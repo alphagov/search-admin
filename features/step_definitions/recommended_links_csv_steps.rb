8ee29d5092167bd4ac7babd309bd92a043207de1
Christian Sutter <git@doublesignal.com>
2024-04-23 13:48:27 +0000

    Remove all Cucumber features and support files

--- features/step_definitions/recommended_links_csv_steps.rb
+++
@@ -1,9 +0,0 @@
-When(/^I view the external links CSV$/) do
-  visit recommended_links_path
-
-  click_on "Download CSV"
-end
-
-Then(/^I should see all external links listed in the CSV$/) do
-  check_for_recommended_links_in_csv_format(@recommended_links)
-end
