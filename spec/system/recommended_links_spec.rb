4f27a604f14376b9a4b97fa6fb53c89227e3a155
Christian Sutter <git@doublesignal.com>
2024-04-23 13:01:09 +0000

    Recommended links: Add system tests for editing

--- spec/system/recommended_links_spec.rb
+++ spec/system/recommended_links_spec.rb
@@ -3,6 +3,8 @@ RSpec.describe "Recommended links" do
     # TODO: Refactor me out into a support file (once we have more system specs and old
     # controller specs are gone that this might conflict with)
     GDS::SSO.test_user = create(:user)
+
+    stub_external_content_publisher
   end
 
   scenario "Viewing recommended links" do
@@ -12,6 +14,45 @@ RSpec.describe "Recommended links" do
 
     then_all_recommended_links_are_displayed
     and_i_can_click_through_to_see_more_details
+  end
+
+  scenario "Editing an existing recommended link" do
+    given_a_recommended_link
+
+    when_i_go_to_view_the_recommended_link
+    and_i_choose_to_edit_it
+    and_i_submit_the_form_with_updated_details
+
+    then_the_link_has_been_updated_locally
+    and_the_changes_have_been_published
+    and_i_can_see_the_new_title
+  end
+
+  scenario "Attempting to edit a recommended link with invalid data" do
+    given_a_recommended_link
+
+    when_i_go_to_view_the_recommended_link
+    and_i_choose_to_edit_it
+    and_i_submit_the_form_with_invalid_details
+
+    then_the_link_has_not_been_updated
+    and_it_has_not_been_published
+    and_i_can_see_what_errors_i_need_to_fix
+  end
+
+  def stub_external_content_publisher
+    allow(ExternalContentPublisher).to receive(:publish)
+    allow(ExternalContentPublisher).to receive(:unpublish)
+  end
+
+  def given_a_recommended_link
+    @link = create(
+      :recommended_link,
+      content_id: "00000000-0000-0000-0000-000000000001",
+      title: "Example link",
+      link: "https://example.com",
+      description: "Example description",
+    )
   end
 
   def given_several_recommended_links
@@ -37,6 +78,31 @@ RSpec.describe "Recommended links" do
     click_on "External links"
   end
 
+  def when_i_go_to_view_the_recommended_link
+    visit recommended_links_path
+    click_on "Example link"
+  end
+
+  def and_i_choose_to_edit_it
+    click_on "Edit"
+  end
+
+  def and_i_submit_the_form_with_updated_details
+    fill_in "Title", with: "Updated title"
+    fill_in "Link", with: "https://example.com/updated"
+    fill_in "Description", with: "Updated description"
+    fill_in "Keywords", with: "Updated keywords"
+    fill_in "Comment", with: "Updated comments"
+    click_on "Save"
+  end
+
+  def and_i_submit_the_form_with_invalid_details
+    fill_in "Title", with: ""
+    fill_in "Link", with: "NOTALINK"
+    fill_in "Description", with: ""
+    click_on "Save"
+  end
+
   def then_all_recommended_links_are_displayed
     expect(page).to have_link("Example link 1")
     expect(page).to have_link("Example link 2")
@@ -45,5 +111,40 @@ RSpec.describe "Recommended links" do
   def and_i_can_click_through_to_see_more_details
     click_on "Example link 1"
     expect(page).to have_selector("h1", text: "Example link 1")
+  end
+
+  def then_the_link_has_been_updated_locally
+    @link.reload
+    expect(@link.title).to eq("Updated title")
+    expect(@link.link).to eq("https://example.com/updated")
+    expect(@link.description).to eq("Updated description")
+    expect(@link.keywords).to eq("Updated keywords")
+    expect(@link.comment).to eq("Updated comments")
+  end
+
+  def and_the_changes_have_been_published
+    expect(ExternalContentPublisher).to have_received(:publish).with(@link)
+  end
+
+  def and_i_can_see_the_new_title
+    expect(page).to have_selector("h1", text: "Updated title")
+  end
+
+  def then_the_link_has_not_been_updated
+    @link.reload
+    expect(@link.title).to eq("Example link")
+    expect(@link.link).to eq("https://example.com")
+    expect(@link.description).to eq("Example description")
+  end
+
+  def and_it_has_not_been_published
+    expect(ExternalContentPublisher).not_to have_received(:publish)
+  end
+
+  def and_i_can_see_what_errors_i_need_to_fix
+    expect(page).to have_content("There is a problem")
+    expect(page).to have_content("Title can't be blank")
+    expect(page).to have_content("Link is an invalid URL")
+    expect(page).to have_content("Description can't be blank")
   end
 end
