3add178fcbaee45943468e3f01d877e6f4011bbd
Christian Sutter <git@doublesignal.com>
2024-04-23 12:48:22 +0000

    Set `rack_test` driver for system tests
    For the time being we don't have any important Javascript-based
    functionality, so change the default driver from Selenium to the much
    more efficient `rack_test`. We can add a tag if or when we do have tests
    that need Javascript.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -31,6 +31,10 @@ RSpec.configure do |config|
 
   config.include FactoryBot::Syntax::Methods
 
+  config.before(:each, type: :system) do
+    driven_by :rack_test
+  end
+
   config.before(:each, type: "controller") do
     login_as_stub_user
   end
