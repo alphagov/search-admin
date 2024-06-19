397c5ab9408ddd18633cb9ed9150a30c8ffe1758
Christian Sutter <git@doublesignal.com>
2024-06-19 13:33:15 +0000

    Add ability to create a new control

--- app/controllers/controls_controller.rb
+++ app/controllers/controls_controller.rb
@@ -6,4 +6,23 @@ class ControlsController < ApplicationController
   def show
     @control = Control.find(params[:id])
   end
+
+  def new
+    @control = Control.new
+  end
+
+  def create
+    @control = Control.new(control_params)
+    if @control.save
+      redirect_to @control, notice: "Control created successfully"
+    else
+      render "new"
+    end
+  end
+
+private
+
+  def control_params
+    params.require(:control).permit(:name, :active, :boost_amount, :filter)
+  end
 end
--- spec/system/controls_spec.rb
+++ spec/system/controls_spec.rb
@@ -15,6 +15,24 @@ RSpec.describe "Controls" do
     then_i_can_see_the_details_of_the_control
   end
 
+  scenario "Creating a new control" do
+    when_i_visit_the_controls_page
+    and_i_choose_to_create_a_new_control
+    and_i_submit_the_form_with_valid_details
+
+    then_the_control_has_been_created
+    and_i_can_see_its_details
+  end
+
+  scenario "Attempting to create a control with invalid data" do
+    when_i_visit_the_controls_page
+    and_i_choose_to_create_a_new_control
+    and_i_submit_the_form_with_invalid_details
+
+    then_the_control_has_not_been_created
+    and_i_can_see_what_errors_i_need_to_fix
+  end
+
   def given_several_controls
     @control1 = create(:control, name: "Control 1")
     @control2 = create(:control, name: "Control 2")
@@ -40,6 +58,35 @@ RSpec.describe "Controls" do
     click_on "Control"
   end
 
+  def and_i_choose_to_create_a_new_control
+    click_on "New control"
+  end
+
+  def and_i_submit_the_form_with_valid_details
+    fill_in "Name", with: "New control"
+    fill_in "Boost amount", with: 0.42
+    fill_in "Filter", with: 'foo: ANY("bar")'
+    check "Activate control"
+    click_on "Save"
+  end
+
+  def and_i_submit_the_form_with_invalid_details
+    fill_in "Name", with: ""
+    fill_in "Boost amount", with: 42.0
+    fill_in "Filter", with: 'foo: ANY("bar")'
+    check "Activate control"
+    click_on "Save"
+  end
+
+  def then_the_control_has_not_been_created
+    expect(Control.count).to eq(0)
+  end
+
+  def and_i_can_see_what_errors_i_need_to_fix
+    expect(page).to have_content("Name can't be blank")
+    expect(page).to have_content("Boost amount must be less than or equal to 1.0")
+  end
+
   def then_all_controls_are_displayed
     expect(page).to have_link("Control 1")
     expect(page).to have_link("Control 2")
@@ -47,5 +94,13 @@ RSpec.describe "Controls" do
 
   def then_i_can_see_the_details_of_the_control
     expect(page).to have_selector("h1", text: "Control")
+  end
+
+  def then_the_control_has_been_created
+    expect(Control.count).to eq(1)
+  end
+
+  def and_i_can_see_its_details
+    expect(page).to have_selector("h1", text: "New control")
   end
 end
