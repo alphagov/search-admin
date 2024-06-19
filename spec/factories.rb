f47fa92672215ab48efe110e51fc663afbaa28d1
Christian Sutter <git@doublesignal.com>
2024-06-19 13:59:52 +0000

    Remove factories for removed models

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,39 +1,4 @@
 FactoryBot.define do
-  sequence :numeric_position do |n|
-    n
-  end
-
-  factory :bet do
-    link { "/death-and-taxes" }
-    expiration_date { Time.zone.now + 1.day }
-
-    trait(:worst) do
-      is_best { false }
-    end
-
-    trait(:best) do
-      is_best { true }
-      position { generate :numeric_position }
-    end
-
-    trait(:permanent) do
-      permanent { true }
-    end
-
-    user
-  end
-
-  factory :query do
-    query { "tax" }
-    match_type { "exact" }
-
-    trait :with_best_bet do
-      after(:create) do |query|
-        create(:bet, :best, query:)
-      end
-    end
-  end
-
   factory :recommended_link do
     title { "Tax online" }
     link { "https://www.tax.service.gov.uk/" }
