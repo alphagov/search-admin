705e45cabdab1741da94679c5cb5c3472e206ba0
Christian Sutter <git@doublesignal.com>
2024-06-19 13:11:03 +0000

    Add ability to view an individual control

--- spec/system/controls_spec.rb
+++ spec/system/controls_spec.rb
@@ -7,9 +7,21 @@ RSpec.describe "Controls" do
     then_all_controls_are_displayed
   end
 
+  scenario "Viewing a single control" do
+    given_a_control
+
+    when_i_go_to_view_the_control
+
+    then_i_can_see_the_details_of_the_control
+  end
+
   def given_several_controls
     @control1 = create(:control, display_name: "Control 1")
     @control2 = create(:control, display_name: "Control 2")
+  end
+
+  def given_a_control
+    @control = create(:control, display_name: "Control")
   end
 
   def when_i_visit_the_controls_page
@@ -17,8 +29,17 @@ RSpec.describe "Controls" do
     click_on "Controls"
   end
 
+  def when_i_go_to_view_the_control
+    visit controls_path
+    click_on "Control"
+  end
+
   def then_all_controls_are_displayed
     expect(page).to have_link("Control 1")
     expect(page).to have_link("Control 2")
+  end
+
+  def then_i_can_see_the_details_of_the_control
+    expect(page).to have_selector("h1", text: "Control")
   end
 end
