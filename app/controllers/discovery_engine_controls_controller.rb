14f81f9c876f9afaf646d1b85e00f4800ee40ac6
Christian Sutter <git@doublesignal.com>
2025-01-13 11:18:50 +0000

    Add ability to view individual DE control

--- app/controllers/discovery_engine_controls_controller.rb
+++ app/controllers/discovery_engine_controls_controller.rb
@@ -1,5 +1,15 @@
 class DiscoveryEngineControlsController < ApplicationController
+  before_action :set_discovery_engine_control, only: %i[show]
+
   def index
     @discovery_engine_controls = DiscoveryEngineControl.all
+  end
+
+  def show; end
+
+private
+
+  def set_discovery_engine_control
+    @discovery_engine_control = DiscoveryEngineControl.find(params.expect(:id))
   end
 end
--- spec/system/discovery_engine_controls_spec.rb
+++ spec/system/discovery_engine_controls_spec.rb
@@ -7,9 +7,22 @@ RSpec.describe "Discovery Engine controls" do
     then_all_discovery_engine_controls_are_displayed
   end
 
+  scenario "Viewing a single Discovery Engine control" do
+    given_a_discovery_engine_boost_control
+
+    when_i_visit_the_discovery_engine_controls_page
+    and_i_click_on_the_discovery_engine_boost_control
+
+    then_i_can_see_the_details_of_the_discovery_engine_control
+  end
+
   def given_several_discovery_engine_controls
     create(:discovery_engine_filter_control, name: "Control 1")
     create(:discovery_engine_boost_control, name: "Control 2")
+  end
+
+  def given_a_discovery_engine_boost_control
+    create(:discovery_engine_boost_control, name: "Boost control")
   end
 
   def when_i_visit_the_discovery_engine_controls_page
@@ -17,8 +30,18 @@ RSpec.describe "Discovery Engine controls" do
     click_on DiscoveryEngineControl.model_name.human.pluralize
   end
 
+  def and_i_click_on_the_discovery_engine_boost_control
+    click_on "Boost control"
+  end
+
   def then_all_discovery_engine_controls_are_displayed
     expect(page).to have_content("Control 1")
     expect(page).to have_content("Control 2")
+  end
+
+  def then_i_can_see_the_details_of_the_discovery_engine_control
+    expect(page).to have_content("Name Boost control")
+    expect(page).to have_content("Active true")
+    expect(page).to have_content("Action boost")
   end
 end
