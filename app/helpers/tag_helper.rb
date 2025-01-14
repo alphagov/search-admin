bab72c89832606d6ff0fa603273166768c15af52
Christian Sutter <git@doublesignal.com>
2025-01-14 13:14:52 +0000

    Remove unused `TagHelper`
    This seems to have been migrated into the `common/page_title` partial at
    some point.

--- app/helpers/tag_helper.rb
+++
@@ -1,7 +0,0 @@
-module TagHelper
-  # Set the page <title> and return the <h1> tag.
-  def page_title(string)
-    content_for :page_title, "#{string} - Search Admin"
-    tag.h1 string, class: "page-title-with-border"
-  end
-end
