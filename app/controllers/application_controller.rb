4653248b9dabca5e86b63579b078d544c236f2b9
Christian Sutter <git@doublesignal.com>
2025-01-23 11:44:49 +0000

    Improve `User` model
    - Move `ApplicationController#admin_user?` into the model as
      `User#admin?`
    - Add some actual tests for `User` model (including the GDS SSO shared
      example suite)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,4 @@ class ApplicationController < ActionController::Base
 
   include GDS::SSO::ControllerMethods
   before_action :authenticate_user!
-
-  def admin_user?
-    current_user.permissions.include?("admin")
-  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ApplicationRecord
+  ADMIN_PERMISSION_KEY = "admin".freeze
+
   include GDS::SSO::User
 
   serialize :permissions, type: Array
+
+  def admin?
+    permissions.include?(ADMIN_PERMISSION_KEY)
+  end
 end
