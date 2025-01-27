4d91cc9ebbce3615b34171f6d4a4184125962f30
Christian Sutter <git@doublesignal.com>
2025-01-27 15:15:03 +0000

    Move `ApplicationController#admin_user?` into `User`
    This is a model concern and shouldn't be on the controller. Also adds
    tests.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,12 @@
 class User < ApplicationRecord
+  # GDS SSO key for administrative permissions
+  ADMIN_PERMISSION_KEY = "admin".freeze
+
   include GDS::SSO::User
 
   serialize :permissions, type: Array
+
+  def admin?
+    permissions.include?(ADMIN_PERMISSION_KEY)
+  end
 end
