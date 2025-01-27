6009ed2812cf22d6f81c5af2b9a38627797b8206
Christian Sutter <git@doublesignal.com>
2025-01-27 15:15:03 +0000

    Move `ApplicationController#admin_user?` into `User`
    This is a model concern and shouldn't be on the controller. Also adds
    tests.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,4 +2,20 @@ require "gds-sso/lint/user_spec"
 
 RSpec.describe User, type: :model do
   it_behaves_like "a gds-sso user class"
+
+  describe "#admin?" do
+    subject(:user) { build_stubbed(:user, permissions:) }
+
+    context "when the user has the `admin` permission" do
+      let(:permissions) { %w[admin and others] }
+
+      it { is_expected.to be_admin }
+    end
+
+    context "when the user does not have the `admin` permission" do
+      let(:permissions) { %w[blah] }
+
+      it { is_expected.not_to be_admin }
+    end
+  end
 end
