194475d8c6a5b9d2d176462024caa14e58fe30e4
Christian Sutter <git@doublesignal.com>
2025-02-18 15:20:13 +0000

    Add control info to serving config show page
    This adds a summary card of attached controls to the serving config show
    page, grouped by control type.

--- spec/system/serving_configs_spec.rb
+++ spec/system/serving_configs_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe "Serving configs", type: :system do
     when_i_visit_the_serving_configs_page
     then_i_should_see_all_serving_configs
     and_i_can_click_through_to_a_serving_config
+    and_i_can_see_its_attached_controls
   end
 
   def given_several_serving_configs_exist
@@ -13,6 +14,10 @@ RSpec.describe "Serving configs", type: :system do
       use_case: :live,
       remote_resource_id: "live-serving-config",
       display_name: "Live serving config",
+      controls: [
+        create(:control, :with_boost_action, display_name: "My live boost control"),
+        create(:control, :with_filter_action, display_name: "My live filter control"),
+      ],
     )
     @preview = create(
       :serving_config,
@@ -34,5 +39,10 @@ RSpec.describe "Serving configs", type: :system do
   def and_i_can_click_through_to_a_serving_config
     click_link "Live serving config"
     expect(page).to have_selector("h1", text: "Live serving config")
+  end
+
+  def and_i_can_see_its_attached_controls
+    expect(page).to have_link("My live boost control")
+    expect(page).to have_link("My live filter control")
   end
 end
