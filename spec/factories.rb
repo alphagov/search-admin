3fad05745e259414c5cc1546aaa44bb3e81ad549
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2025-06-18 11:11:48 +0100

    Remove control models

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,28 +1,4 @@
 FactoryBot.define do
-  factory :control do
-    display_name { "Control" }
-    comment { "This is a nice control." }
-
-    action factory: :control_boost_action
-
-    trait :with_boost_action do
-      action factory: :control_boost_action
-    end
-
-    trait :with_filter_action do
-      action factory: :control_filter_action
-    end
-  end
-
-  factory :control_boost_action, class: Control::BoostAction do
-    filter_expression { 'link: ANY("/example")' }
-    boost_factor { 0.13 }
-  end
-
-  factory :control_filter_action, class: Control::FilterAction do
-    filter_expression { 'link: ANY("/example")' }
-  end
-
   factory :completion_denylist_entry do
     phrase { "tea time" }
     match_type { :contains }
