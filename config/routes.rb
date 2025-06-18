fd1f1153598b337ca6124836eddc69218dea69f9
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2025-06-18 11:14:03 +0100

    Remove control routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,13 +6,6 @@ Rails.application.routes.draw do
   )
   mount GovukPublishingComponents::Engine, at: "/component-guide" if Rails.env.development?
 
-  resources :controls, except: %i[new]
-  scope :controls, only: %i[new], as: "control" do
-    # NOTE: Rails does not accept :controller as an argument on `scope`, so we need to duplicate
-    # it in every resource definition in this scope.
-    resources :with_boost_actions, controller: :controls, action_type: Control::BoostAction
-    resources :with_filter_actions, controller: :controls, action_type: Control::FilterAction
-  end
   resources :completion_denylist_entries, except: %i[show]
   resources :completion_denylist_entry_imports, only: %i[new create]
 
